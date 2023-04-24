6684b08ac75f4f4bb0f67ac07a6fa56ec935b4a6
Ezi <etsogli@uncc.edu>
2023-04-23 21:01:45 -0400

    Formatting

--- main.tf
+++ main.tf
@@ -40,7 +40,7 @@ resource "azurerm_storage_account" "storage" {
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-  tags = local.tags
+  tags                     = local.tags
 }
 
 resource "azurerm_application_insights" "ai" {
