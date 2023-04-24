c92fd4ae89690d186c48498a923e55de10f7bb64
Ezi <etsogli@uncc.edu>
2023-04-23 20:57:32 -0400

    Pushing main tail

--- main.tf
+++ main.tf
@@ -14,6 +14,7 @@ locals {
 data "azurerm_subscription" "current" {
 }
 
+data "azurerm_client_config" "current" {}
 // Random Suffix Generator
 
 resource "random_integer" "deployment_id_suffix" {
@@ -39,6 +40,36 @@ resource "azurerm_storage_account" "storage" {
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-
   tags = local.tags
 }
+
+resource "azurerm_application_insights" "ai" {
+  name                = "${var.class_name}-${var.student_name}-${var.environment}-${random_integer.deployment_id_suffix.result}-ai"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  application_type    = "web"
+}
+
+resource "azurerm_key_vault" "keyvault" {
+  name                = "${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}keyvault"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+  sku_name            = "premium"
+}
+
+resource "azurerm_machine_learning_workspace" "mlw" {
+  name                    = "${var.class_name}-${var.student_name}-${var.environment}-${random_integer.deployment_id_suffix.result}-mlw"
+  location                = azurerm_resource_group.rg.location
+  resource_group_name     = azurerm_resource_group.rg.name
+  storage_account_id      = azurerm_storage_account.storage.id
+  application_insights_id = azurerm_application_insights.ai.id
+  key_vault_id            = azurerm_key_vault.keyvault.id
+
+  tags = local.tags
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
